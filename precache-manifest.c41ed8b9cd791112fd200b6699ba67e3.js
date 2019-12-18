6764bfba993581093e788c60ef26da69784c5365
Maayan Albert <maayan@Maayans-MBP.fios-router.home>
2019-12-17 22:50:13 -0500

    Updates

---
+++ precache-manifest.c41ed8b9cd791112fd200b6699ba67e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5987cdec7b901e52afe87fb355608ec",
+    "url": "/genderbender/index.html"
+  },
+  {
+    "revision": "5b83bf10eba6b65279be",
+    "url": "/genderbender/static/css/main.c7cae342.chunk.css"
+  },
+  {
+    "revision": "01fd9252c0f9af5108e1",
+    "url": "/genderbender/static/js/2.c45344bc.chunk.js"
+  },
+  {
+    "revision": "5b83bf10eba6b65279be",
+    "url": "/genderbender/static/js/main.acc42fa9.chunk.js"
+  },
+  {
+    "revision": "97019413c1b01352e046",
+    "url": "/genderbender/static/js/runtime-main.b5be5fef.js"
+  }
+]);
